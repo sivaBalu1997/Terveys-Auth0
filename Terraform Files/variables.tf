ccec30e0d0ee21dd19213dd5edbf5f9617c7f8d5
sivaBalu1997 <128580030+sivaBalu1997@users.noreply.github.com>
2024-03-17 12:45:28 +0530

    Variables file

---
+++ Terraform Files/variables.tf
@@ -0,0 +1,14 @@
+variable "auth0_domain" {
+  type        = string
+  description = "Enter the Auth0 domain name"
+}
+
+variable "auth0_client_id" {
+  type        = string
+  description = "Enter the Auth0 client ID"
+}
+
+variable "auth0_client_secret" {
+  type        = string
+  description = "Enter the Auth0 client secret"
+}
